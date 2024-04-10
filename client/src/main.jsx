3d8127cb6d265a339e9087822b35bb1439118256
Nisson <nisson2000@gmail.com>
2024-04-10 20:32:14 +0800

    404

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,9 +5,24 @@ import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 
+function HandleRedirect() {
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    const redirectPath = sessionStorage.redirect;
+    delete sessionStorage.redirect;
+    if (redirectPath) {
+      navigate(redirectPath);
+    }
+  }, [navigate]);
+
+  return null;
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <BrowserRouter>
+            <HandleRedirect />
             <AuthProvider>
                 <App />
             </AuthProvider>
